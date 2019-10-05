01ee026194b84b39e14a2a4f2f665ea4e9ef568f
samir <allabsamir777@gmail.com>
2019-10-05 11:08:22 +0200

    install JWTRefreshTokenBundle for refresh a expire token

--- src/Controller/CustomerController.php
+++ src/Controller/CustomerController.php
@@ -3,12 +3,20 @@
 namespace App\Controller;
 
 use App\Entity\Customer;
+use Symfony\Component\HttpFoundation\Request;
+use Doctrine\Common\Persistence\ObjectManager;
+use Symfony\Component\HttpFoundation\Response;
 use FOS\RestBundle\Controller\FOSRestController;
 use FOS\RestBundle\Controller\Annotations as Rest;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class CustomerController extends FOSRestController
 {
-    
+    private $encoder;
+    public function __construct(UserPasswordEncoderInterface $encoder)
+    {
+        $this->encoder = $encoder;
+    }
     /**
      * Undocumented function
      *
@@ -19,5 +27,30 @@ class CustomerController extends FOSRestController
     public function getCustomer(Customer $customer)
     {
         return $customer;
+    }
+    /**
+     * 
+     * @return void
+     * 
+     * @Rest\Post(path ="/customers", name="app_customer_create")
+     * @Rest\View()
+     */
+    public function register(Request $request,ObjectManager $em)
+    {
+        $username = $request->request->get('username');
+        $password = $request->request->get('password');
+        $fullname = $request->request->get('fullname');
+        $customer = new Customer();
+        $customer->setFullname($fullname);
+        $customer->setUsername($username);
+        $customer->setRoles(['ROLE_USER']);
+        $customer->setPassword($this->encoder->encodePassword($customer, $password));
+        $em->persist($customer);
+        $em->flush();
+        return New Response(sprintf('Customer %s successfully created', $customer->getUsername()),Response::HTTP_CREATED);
+    }
+    public function api()
+    {
+        return new Response(sprintf('Logged in as %s', $this->getUser()->getUsername()));
     }
 }
