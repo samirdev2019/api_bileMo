5cd49f991e9d1a582c94cbb030d858d6f5f95fe0
samir <allabsamir777@gmail.com>
2019-10-09 22:54:55 +0200

    instaltion of nelmio cors-bundle

--- config/bundles.php
+++ config/bundles.php
@@ -22,4 +22,5 @@ return [
     Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
     Gesdinet\JWTRefreshTokenBundle\GesdinetJWTRefreshTokenBundle::class => ['all' => true],
     Nelmio\ApiDocBundle\NelmioApiDocBundle::class => ['all' => true],
+    Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
 ];
