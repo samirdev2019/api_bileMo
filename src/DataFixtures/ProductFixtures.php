bc2354b346f7471c0115afb36db11aa3551913af
samir <allabsamir777@gmail.com>
2019-10-13 09:52:08 +0200

    update dataFixtures

--- src/DataFixtures/ProductFixtures.php
+++ src/DataFixtures/ProductFixtures.php
@@ -5,25 +5,25 @@ namespace App\DataFixtures;
 use App\Entity\Product;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
-
+use Faker;
 class ProductFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
-
-        for($i=0; $i<=20; $i++) {
+        $faker = Faker\Factory::create('fr_FR');
+        for($i=0; $i<=100; $i++) {
             $product = new Product();
-            $product->setMark("Sumsung Note$i");
-            $product->setReference("P$i Smart 2019");
-            $product->setDescription("I'm an description of the product");
-            $product->setStorageCapacityROM("160 Go");
-            $product->setMemory("4 Go");
-            $product->setMaxMemoryCard("255 GO");
-            $product->setScreenSize("6\"");
-            $product->setPrincipalCameraResolution("12Mpx");
-            $product->setSecondCameraResolution("8 Mpx");
+            $product->setMark("Sumsung");
+            $product->setReference($faker->bothify('Sumsung ##??'));
+            $product->setDescription($faker->sentence($nbWords = 6, $variableNbWords = true));
+            $product->setStorageCapacityROM($faker->numberBetween($min = 32, $max = 160)." GO");
+            $product->setMemory($faker->numberBetween($min = 1, $max = 12)." GO");
+            $product->setMaxMemoryCard($faker->numberBetween($min = 32, $max = 255).'Go');
+            $product->setScreenSize($faker->randomDigit.'"');
+            $product->setPrincipalCameraResolution($faker->numberBetween($min = 5, $max = 20)."Mpx");
+            $product->setSecondCameraResolution($faker->numberBetween($min = 5, $max = 20)."Mpx");
             $product->setColor(true);
-            $product->setPrice(mt_rand(100, 500));
+            $product->setPrice($faker->randomNumber(2));
             $product->setOperatingSystem("ANDROID");
             $product->setProcessor("3X1.53 GHz");
             $product->setCreatedAt(new \DateTime());
